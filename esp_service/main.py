4102903dbfe278de14a8999ceb19cc800e4a6917
Григорий Алексеев <ggangstar36@gmail.com>
2026-02-14 16:41:49 +0400

    update CORS

--- esp_service/main.py
+++ esp_service/main.py
@@ -111,7 +111,10 @@ app = FastAPI(lifespan=lifespan, title="ESP Ядро")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tgapp.dotnetdon.ru:4443"],  # или ["*"] для теста
+    allow_origins=[
+        "https://tgapp.dotnetdon.ru:4443",
+        "http://localhost:5173",  # для локальной разработки
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
