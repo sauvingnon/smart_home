f5aef41c1af4c30be3aa02766adb7a2d76b23ab4
Гриша Шкробов <ggangstar36@gmail.com>
2026-08-13 13:26:08 +0400

    fix: таймаут включения дневного нужен больше

--- esp_front/src/pages/CameraPage/CameraPage.tsx
+++ esp_front/src/pages/CameraPage/CameraPage.tsx
@@ -205,7 +205,7 @@ export const CameraPage: React.FC = () => {
         manualNightState: false,
       })
 
-      await new Promise((resolve) => setTimeout(resolve, 1400))
+      await new Promise((resolve) => setTimeout(resolve, 2000))
 
       await apiClient.updateSettings(original)
     } catch (e) {
